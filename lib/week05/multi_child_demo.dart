dd138c402febe4a83693e1a7a2fc046472468ce9
NurulhudaAi <Nurulhudaadamishaq@gmail.com>
2025-09-11 15:08:52 +0700

    added multiple Child

---
+++ lib/week05/multi_child_demo.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MultiChildDemo extends StatefulWidget {
+  const MultiChildDemo({super.key});
+
+  @override
+  State<MultiChildDemo> createState() => _MultiChildDemoState();
+}
+
+class _MultiChildDemoState extends State<MultiChildDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Container(color: Colors.green, width: 100, height: 100),
+            Container(color: Colors.yellow, width: 100, height: 150),
+            Container(color: Colors.red, width: 100, height: 100),
+          ],
+        ),
+      ),
+    );
+  }
+}
