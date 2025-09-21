c3bf525d89eae3e6d071f873922b4e98030fa0a8
NurulhudaAi <Nurulhudaadamishaq@gmail.com>
2025-09-21 13:28:50 +0700

    Exercise 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_innit/week06/exersice_6.dart';
+import 'package:flutter_innit/week06/exercise_10.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Exersice6(),
+      home: const Exercise10(),
     );
   }
 }
