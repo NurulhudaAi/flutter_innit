84bb4c9bfb5ee40d246b588d16b365420abec5a6
NurulhudaAi <Nurulhudaadamishaq@gmail.com>
2025-10-10 21:42:56 +0700

    week09

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_innit/week07/sample2.dart';
+import 'package:flutter_innit/week09/assignment_3.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FruitMemoryGame(),
+      home: const CoffeeOrderApp2(),
     );
   }
 }
